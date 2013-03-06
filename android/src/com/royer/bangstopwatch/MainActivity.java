23345831c35c0db82a89ff89e60de4d006950f13
RoyerWang <royerwang@gmail.com>
2013-03-06 09:30:35 -0800

    prepare branch dev

--- android/src/com/royer/bangstopwatch/MainActivity.java
+++ android/src/com/royer/bangstopwatch/MainActivity.java
@@ -4,19 +4,31 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.SherlockActivity;
+
+import com.royer.bangstopwatch.*;
+
+public class MainActivity extends SherlockActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        
     }
 
+    
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    */
     
 }
